91f9fd21045e8143ab4fd5e6e12965cb4efd394b
Andrey <andrey98vas@gmail.com>
2019-11-16 12:35:35 +0300

    Fixed converter for bitmap

--- ConvertMap.py
+++ ConvertMap.py
@@ -1,15 +1,29 @@
 def convert(bitmap, width):
     i=0
     j=0
+    f = open('text.txt', 'w')
+
     newmap=[[]]
     for bit in bitmap:
         newmap[j].append(bit)
         i=i+1
+        if bit:
+            f.write('1')
+        else:
+            f.write('0')
         if i==width:
+            f.write('\n')
             newmap.append([])
             i=0
             j=j+1
+    #for ter in newmap:
+    #    print(ter)
+     #   print('\n')
     return newmap
+
+
+def ConvertAdress(pos, width):
+    return [width-pos//width, pos-pos//width*width]
 
 
 if __name__ == '__main__':
